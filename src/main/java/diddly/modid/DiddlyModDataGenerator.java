7cd341d84b6afe282bf0fa7f5902b91aac0f543c
nodalus <landon.traynor@gmail.com>
2024-10-11 15:02:25 -0400

    Initial Commit

---
+++ src/main/java/diddly/modid/DiddlyModDataGenerator.java
@@ -0,0 +1,11 @@
+package diddly.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DiddlyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/diddly/modid/TutorialModClient.java
@@ -0,0 +1,12 @@
+
+package diddly.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+   @Override
+
+   public void onInitializeClient() {
+
+   }
+}
